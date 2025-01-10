37504cdbedbb2e43dbaf8e865bbe00eb4a3c258a
LeKhaiTri <lekhaitri.2003@gmail.com>
2025-01-10 10:34:59 +0700

    add entity PostStatistic

---
+++ postservicecommand/src/main/java/com/namdam1123/j2ee/postservicecommand/Entities/PostStatistic.java
@@ -0,0 +1,13 @@
+package com.namdam1123.j2ee.postservicecommand.Entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class PostStatistic {
+    public List<UUID> PostId;
+    public int AverageLike;
+    public LocalDateTime StartTime;
+    public LocalDateTime EndTime;
+
+}
